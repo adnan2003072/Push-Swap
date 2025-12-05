2e7bef346460916ea7237075c6a83de632e8e4b2
Adnan Bouzkraoui <abouzkra@e1r3p15.1337.ma>
2025-12-05 16:59:44 +0100

    TODO: improve ft_atoi

--- utils/ft_atoi.c
+++ utils/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abouzkra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:51:09 by abouzkra          #+#    #+#             */
-/*   Updated: 2025/12/05 16:53:29 by abouzkra         ###   ########.fr       */
+/*   Updated: 2025/12/05 16:59:38 by abouzkra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,37 @@ long	ft_atoi(const char *nptr)
 		digit = nptr[i] - '0';
 		if (check_overflow(res, digit, sign))
 			return ((long)int_max + 1);
-		res = res * 10 + digit;
 		i++;
 	}
 	return (sign * res);
+}
+
+long	ft_atoi(const char *s)
+{
+	long	res = 0;
+	int		sign = 1;
+	size_t	i = 0;
+	int int_min = (1 << 31);
+	int int_max = ~int_min;
+	long	overflow_val = (long)int_max + 1;
+
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+		if (sign == 1 && (res > (int_max - digit) / 10))
+			return overflow_val;
+		if (sign == -1 && (-res < (int_min + digit) / 10))
+			return overflow_val;
+		res = res * 10 + digit;
+		i++;
+	}
+	return res * sign;
 }
